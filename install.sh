f721ce06b6773b28df21b2c23e727542882cf19f
Akinori Mizushima <akinori.mizushima@slalom.com>
2021-12-04 23:03:34 +0900

    Add nvm

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install fish -y
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 curl -sL 'https://raw.githubusercontent.com/jorgebucaran/fisher/HEAD/functions/fisher.fish' | source && fisher install jorgebucaran/fisher
