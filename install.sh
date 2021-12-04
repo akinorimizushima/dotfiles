8f0d5bb9dd49f532fec0eb572dca2bb9b4947f23
Akinori Mizushima <akinori.mizushima@slalom.com>
2021-12-04 23:47:34 +0900

    Update install fish

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-add-repository ppa:fish-shell/release-3 -y
+sudo apt-get update
 sudo apt-get install fish -y
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-curl -sL 'https://raw.githubusercontent.com/jorgebucaran/fisher/HEAD/functions/fisher.fish' | source && fisher install jorgebucaran/fisher
-fish
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install edc/bass jethrokuan/fzf FabioAntunes/fish-nvm
-bash
 which fish | chsh
