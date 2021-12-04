17fe6d9f63024fdd0aea31d38930748ac0da6deb
Akinori Mizushima <akinori.mizushima@slalom.com>
2021-12-04 23:14:00 +0900

    Add bass for nvm

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 curl -sL 'https://raw.githubusercontent.com/jorgebucaran/fisher/HEAD/functions/fisher.fish' | source && fisher install jorgebucaran/fisher
 fish
-fisher install jethrokuan/fzf FabioAntunes/fish-nvm
+fisher install edc/bass jethrokuan/fzf FabioAntunes/fish-nvm
 bash
 which fish | chsh
