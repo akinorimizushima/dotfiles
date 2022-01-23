3b3e5595b95956207a41aa4b1ba6ba325f31a7e8
Akinori Mizushima <akinori.mizushima@slalom.com>
2022-01-23 15:16:20 +0900

    Update

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install edc/bass jethrokuan/fzf FabioAntunes/fish-nvm
+fisher install edc/bass jethrokuan/fzf FabioAntunes/fish-nvm decors/fish-gqh pure-fish/pure jethrokuan/z
 which fish | chsh
