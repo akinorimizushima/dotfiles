f751417d1b0c8375ded76ff39156ed7e55d6ce8b
Akinori Mizushima <akinori.mizushima@slalom.com>
2022-01-23 15:51:20 +0900

    Update

--- mac.sh
+++ mac.sh
@@ -2,9 +2,11 @@
 
 # Install apps via homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install fish
-brew install --cask iterm2 google-chrome docker virtualbox vagrant
+brew install git fish
+brew install --cask iterm2 google-chrome docker virtualbox vagrant jetbrains-toolbox visual-studio-code
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+# TODO: Install Source Code Pro font
 
 # Install fish shell
 sh install-fish.sh
